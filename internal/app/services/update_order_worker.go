890c6fee288e45cb2aea0536c31a3a1a20d6785c
AV <alexandervdovenkov@yandex.ru>
2023-07-11 16:08:07 +0300

    Переписал механизм транзакционности

--- internal/app/services/update_order_worker.go
+++ internal/app/services/update_order_worker.go
@@ -16,36 +16,24 @@ func WorkerProcessingOrders(ch <-chan storage.Order, host string, db *gorm.DB) {
 		logger.Log.Infof("processing %v", order)
 		go func(order storage.Order) {
 			getOrderDetails(&order, host)
-			tx := db.Begin() // Начало транзакции
-			updateOrderDetails(&order, tx)
-			updateUserBalance(order.UserID, order.Accrual, tx)
-			if err := tx.Commit().Error; err != nil {
-				tx.Rollback() // Откат транзакции при ошибке
+			err := db.Transaction(func(tx *gorm.DB) error {
+				if err := tx.Model(order).Updates(order).Error; err != nil {
+					return err
+				}
+				var savedUser storage.User
+				if err := tx.First(&savedUser, "id = ?", order.UserID).Error; err != nil {
+					return err
+				}
+				savedUser.Balance += order.Accrual
+				if err := tx.Updates(&savedUser).Error; err != nil {
+					return err
+				}
+				return nil
+			})
+			if err != nil {
 				logger.Log.Infof("Failed to process order %v: %v", order.ID, err)
 			}
 		}(order)
-	}
-}
-
-func updateOrderDetails(order *storage.Order, tx *gorm.DB) {
-	err := tx.Model(order).Updates(order).Error
-	if err != nil {
-		logger.Log.Infof("Failed to update order %v: %v", order.ID, err)
-	}
-}
-
-func updateUserBalance(userID uint, accrual float64, tx *gorm.DB) {
-	var savedUser storage.User
-	err := tx.First(&savedUser, "id = ?", userID)
-	if err != nil {
-		logger.Log.Infof("Failed to find user %v: %v", userID, err)
-		tx.Rollback()
-	}
-	savedUser.Balance += accrual
-	tx.Updates(&savedUser)
-	if tx.Error != nil {
-		logger.Log.Infof("Failed to update user %v: %v", userID, err)
-		tx.Rollback()
 	}
 }
 
